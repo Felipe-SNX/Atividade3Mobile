46289956f04b606645ef161047340576a2bb7e85
unknown <felipesantossnx@gmail.com>
2025-06-09 20:46:08 -0300

    Layout tela de conversão

---
+++ app/src/main/java/com/example/conversordemoeda/ConverterRecursosActivity.kt
@@ -0,0 +1,112 @@
+package com.example.conversordemoeda
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Spinner
+import android.widget.TextView
+import com.example.conversordemoeda.data.Moeda
+import com.example.conversordemoeda.interfaces.MoedaApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.DecimalFormat
+
+class ConverterRecursosActivity : AppCompatActivity() {
+
+    private lateinit var comboOrigem: Spinner;
+    private lateinit var comboDestino: Spinner;
+    private lateinit var editTextValor: EditText;
+    private lateinit var textViewMoedaOrigem: TextView;
+    private lateinit var textViewMoedaDestino: TextView;
+    private lateinit var btnConverter: Button;
+    private lateinit var progressBar: ProgressBar;
+    private lateinit var moedaApi: MoedaApi;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_converter_recursos)
+
+        editTextValor = findViewById(R.id.editTextValor);
+        textViewMoedaOrigem = findViewById(R.id.textViewMoedaOrigem);
+        textViewMoedaDestino = findViewById(R.id.textViewMoedaDestino);
+        btnConverter = findViewById(R.id.btnConverter);
+        progressBar = findViewById(R.id.progressBar);
+        comboOrigem = findViewById(R.id.spinnerMoedaOrigem);
+        comboDestino = findViewById(R.id.spinnerMoedaDestino);
+
+        val opcoesFiltro = listOf("Bitcoin", "Dólar", "Real");
+        val comboAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opcoesFiltro);
+        comboOrigem.adapter = comboAdapter;
+        comboDestino.adapter = comboAdapter;
+
+        adicionaListenerValor();
+
+        //Configura retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://economia.awesomeapi.com.br/json/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+        moedaApi = retrofit.create(MoedaApi::class.java);
+
+    }
+
+    fun converterMoeda(view: View){
+        val moeda: Moeda = buscarDadoApi();
+    }
+
+    fun buscarDadoApi(): Moeda{
+
+    }
+
+    fun adicionaListenerValor(){
+        //Formata o valor para duas casas decimais
+        editTextValor.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                editTextValor.removeTextChangedListener(this);
+
+                val stringOriginal = editable.toString();
+                val separadorDecimal = DecimalFormat().decimalFormatSymbols.decimalSeparator;
+                var textoLimpo = stringOriginal.replace(".", separadorDecimal.toString());
+
+                val temVirgula = textoLimpo.indexOf(separadorDecimal);
+
+                if (temVirgula >= 0) {
+                    val integerPart = textoLimpo.substring(0, temVirgula);
+                    var decimalPart = textoLimpo.substring(temVirgula + 1);
+
+                    if (decimalPart.length > 2) {
+                        decimalPart = decimalPart.substring(0, 2);
+                    }
+
+                    val newText = "$integerPart$separadorDecimal$decimalPart";
+                    editTextValor.setText(newText);
+                    editTextValor.setSelection(newText.length);
+                } else {
+                    editTextValor.setText(textoLimpo);
+                    editTextValor.setSelection(textoLimpo.length);
+                }
+
+                editTextValor.addTextChangedListener(this)
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/conversordemoeda/MainActivity.kt
+++ app/src/main/java/com/example/conversordemoeda/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.conversordemoeda
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        button = findViewById<Button>(R.id.converterButton);
+    }
+
+    fun irParaConversao(view: View){
+        startActivity(Intent(this, ConverterRecursosActivity::class.java))
     }
 }
